1ba5383a4095c28df02235785a04874915cf48a2
takao-h <hayashi_takao@cyberbuzz.co.jp>
2022-01-24 00:22:12 +0900

    Added sorter

--- sound_plugin/lib/sound_plugin.dart
+++ sound_plugin/lib/sound_plugin.dart
@@ -2,6 +2,9 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:import_sorter/args.dart';
+import 'package:import_sorter/files.dart';
+import 'package:import_sorter/sort.dart';
 
 class SoundPlugin {
   static const MethodChannel _channel = MethodChannel('sound_plugin');
