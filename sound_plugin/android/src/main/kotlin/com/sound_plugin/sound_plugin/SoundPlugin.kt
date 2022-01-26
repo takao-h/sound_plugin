fd59b4631316c4c6086d3096b79ed8b7fa74276c
takao-h <hayashi_takao@cyberbuzz.co.jp>
2022-01-26 23:57:38 +0900

    WIP

--- sound_plugin/android/src/main/kotlin/com/sound_plugin/sound_plugin/SoundPlugin.kt
+++ sound_plugin/android/src/main/kotlin/com/sound_plugin/sound_plugin/SoundPlugin.kt
@@ -16,20 +16,45 @@ class SoundPlugin: FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sound_plugin")
-    channel.setMethodCallHandler(this)
+//  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+//    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sound_plugin")
+//    channel.setMethodCallHandler(this)
+//  }
+//
+//  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+//    if (call.method == "getPlatformVersion") {
+//      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+//    } else {
+//      result.notImplemented()
+//    }
+//  }
+//
+//  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+//    channel.setMethodCallHandler(null)
+//  }
+
+  companion object {
+    private const val CHANNEL = "sound_plugin"
+    private const val METHOD_GET_LIST = "getList"
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  private lateinit var channel: MethodChannel
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(FlutterEngine)
+
+    channel = MethodChannel(flutterEngine.dartExecutor.binaryMessager, CHANNEL)
+    channel.setMethodCallHander { methodCall: MethodCall, result: MethodChannel.Result ->
+      if(methodCall.method == METHOD_GET_LIST) {
+        val name = methodCall.argument<String>("name").toString()
+        val age = methodCall.argument<Int>("age")
+        Log.d("Android", "name = ${name}, age = $age")
+
+        val list = listOf("data0", "data1", "data2")
+        result.success(list)
+      }
+      else
+        result.notImplemented()
     }
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
   }
 }
--- sound_plugin/lib/sound_plugin.dart
+++ sound_plugin/lib/sound_plugin.dart
@@ -9,8 +9,24 @@ import 'package:import_sorter/sort.dart';
 class SoundPlugin {
   static const MethodChannel _channel = MethodChannel('sound_plugin');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  // static Future<String?> get platformVersion async {
+  //   final String? version = await _channel.invokeMethod('getPlatformVersion');
+  //   return version;
+  // }
+
+  static Future<dynamic> get _list async{
+    final Map params<String, dynamic> {
+      'name': 'my name is hoge',
+      'age': 25,
+    }
+    final list<dynamic> list = await _channel.invokeMethod('getList', params);
+    return list;
+  }
+
+  @override
+  initState(){
+    super initState();
+
+    _list.then((val) => print(val))
   }
 }
